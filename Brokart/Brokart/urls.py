2cf2ec89b9ba2ae2e880655e0ce22416b616856a
sajjadmk <mohammadsajjad383@gmail.com>
2024-07-22 13:14:22 +0530

    brokart project part 21

--- Brokart/Brokart/urls.py
+++ Brokart/Brokart/urls.py
@@ -23,6 +23,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Products.urls')),
+    path('customer/', include('Customers.urls')),
+    path('orders/', include('Orders.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Brokart/Products/urls.py
+++ Brokart/Products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.index, name = 'home'),
-    path('products', views.list_products, name = 'list_products'),
+    path('products_list', views.list_products, name = 'list_products'),
+    path('product_details', views.product_details, name = 'product_details'),
 ]
