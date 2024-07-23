9cd25850f01249f73ec5734041994fd63a27ee86
sajjadmk <mohammadsajjad383@gmail.com>
2024-07-23 14:47:59 +0530

    brokart project part 23

--- Brokart/Products/views.py
+++ Brokart/Products/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from . models import Product
+from django.core.paginator import Paginator
 
 # Create your views here.
 
@@ -6,7 +8,14 @@ def index(request):
     return render(request, 'index.html')
 
 def list_products(request):
-    return render(request, 'products.html')
+    page = 1
+    if request.GET:
+        page = request.GET.get('page', 1)
+
+    products = Product.objects.all()
+    product_paginator=Paginator(products, 12)
+    products=product_paginator.get_page(page)
+    return render(request, 'products.html', {'products' : products})
 
 def product_details(request):
     return render(request, 'product_details.html')
