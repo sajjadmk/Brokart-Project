fdbe924a5bdc74cb743b930eb1c698974a01d89a
sajjadmk <mohammadsajjad383@gmail.com>
2024-08-04 19:18:43 +0530

    brokart project

--- Brokart/Customers/views.py
+++ Brokart/Customers/views.py
@@ -1,6 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from .models import Customer
 
 # Create your views here.
 
 def show_account(request):
+    if request.POST and 'register' in request.POST:
+        try:
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            email = request.POST.get('email')
+            address = request.POST.get('address')
+            phone = request.POST.get('phone')
+
+            user = User.objects.create(
+                username=username,
+                password=password,
+                email=email
+            )
+            
+            customer = Customer.objects.create(
+                user=user,
+                address=address,
+                phone=phone
+            )
+            return redirect('home')
+        except Exception as e:
+            error_message = 'invalid credentials'
+            messages.error(request, error_message)
+
+
     return render(request, 'account.html')
